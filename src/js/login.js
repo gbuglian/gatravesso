aefd96b38b61dd0d12c305c4d38e9ad390625b57
Gbuglian <biel.bugliani@gmail.com>
2022-05-25 17:55:41 -0300

    Inclue validações Tela de Login

--- src/js/login.js
+++ src/js/login.js
@@ -10,42 +10,70 @@ let btnSend = document.querySelector('#btnSend');
 
 let msgErro = document.querySelector('#msg_erro');
 
+async function postData(url ="", data = {}){
+    const response = await fetch(url, {
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        mode: 'cors', // no-cors, *cors, same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: 'same-origin', // include, *same-origin, omit
+        headers: {
+          'Content-Type': 'application/json'
+          // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        redirect: 'follow', // manual, *follow, error
+        referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        body: JSON.stringify(data) // body data type must match "Content-Type" header
+      });
+      return response.json(); // parses JSON response into native JavaScript objects
+}
+
+
+function entrar() {
+    if(validEmail && validPassword) {
+      msgErro.innerHTML = '';
+      postData('http://localhost:8081/gatravesso/v1/login', 
+        {
+            usuario: document.querySelector("#input_email").value, 
+            senha: document.querySelector("#input_password").value
+        }).then(data => {
+            console.log(data);
+        })
+    } else{
+        msgErro.setAttribute('style', 'color: red');
+        msgErro.setAttribute('style', 'display:block');
+        msgErro.innerHTML = 'Preencha todos os campos corretamente e tente novamente'
+    }
+
+}
+
 email.addEventListener('keyup', () => {
-    if (email.value.length < 3) {
+    if (email.value.length < 6) {
+        email.setAttribute('style', 'color: red');
+        email.setAttribute('style', 'border: 1px red solid');
         label_email.setAttribute('style', 'color: red');
-        label_email.innerHTML = '*Insira no minimo 3 caracteres'  
+        label_email.innerHTML = '*Insira no minimo 6 caracteres'  
         validEmail = false
     } else {
+        email.setAttribute('style', 'color: black');
+        email.setAttribute('style', 'border: 1px green solid');
         label_email.innerHTML = ''
         validEmail = true
     }
 })
 
 password.addEventListener('keyup', () => {
-    if (password.value.length < 3) {
+    if (password.value.length < 6) {
+        password.setAttribute('style', 'color: red');  
+        password.setAttribute('style', 'border: 1px red solid');
         label_password.setAttribute('style', 'color: red');
-        label_password.innerHTML = '*Insira no minimo 3 caracteres'
+        label_password.innerHTML = '*Insira no minimo 6 caracteres'
         validPassword = false
     } else {
+        password.setAttribute('style', 'border: 1px green solid');
         label_password.innerHTML = ''
         validPassword = true
     }
 })
-
-
-function entrar() {
-    if(validEmail && validPassword) {
-      msgErro.innerHTML = '';
-    } else{
-        msgErro.setAttribute('style', 'color: red');
-        msgErro.setAttribute('style', 'display:block');
-        msgErro.innerHTML = 'Preencha todos os campos corretamente e tente novamente'
-        
-        setTimeout(() => {
-            window.location.reload()
-        }, 2000)
-    }
-}
 
 email.addEventListener('blur', () => {
     email.style.borderColor = "#ccc";

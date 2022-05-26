3d88af967e90997f347f0e4ae6c99a5b0e888478
Gbuglian <biel.bugliani@gmail.com>
2022-05-26 00:25:50 -0300

    Integra tela de login com backend validando usuario e senha

--- src/js/login.js
+++ src/js/login.js
@@ -7,70 +7,75 @@ let labelPassword = document.querySelector('#label_password')
 let validPassword = false
 
 let btnSend = document.querySelector('#btnSend');
-
 let msgErro = document.querySelector('#msg_erro');
 
-async function postData(url ="", data = {}){
-    const response = await fetch(url, {
-        method: 'POST', // *GET, POST, PUT, DELETE, etc.
-        mode: 'cors', // no-cors, *cors, same-origin
-        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-        credentials: 'same-origin', // include, *same-origin, omit
-        headers: {
-          'Content-Type': 'application/json'
-          // 'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        redirect: 'follow', // manual, *follow, error
-        referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-        body: JSON.stringify(data) // body data type must match "Content-Type" header
-      });
-      return response.json(); // parses JSON response into native JavaScript objects
-}
+function validarSenha(url ="", data = {}) {
 
+    fetch(url, { 
+
+        method: 'POST',
+        mode: 'cors',
+        cache: 'no-cache',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        redirect: 'follow',
+        referrerPolicy: 'no-referrer',
+        body: JSON.stringify(data)
+
+    }).then(function(response) {
+
+        if (response.status == 401) {
+            msgErro.setAttribute('style', 'display:block');
+            msgErro.innerHTML = 'Usuário não cadastrado'
+        }
+
+    }).catch(function(error){
+        console.error(error.message);
+    })
+} 
 
 function entrar() {
     if(validEmail && validPassword) {
       msgErro.innerHTML = '';
-      postData('http://localhost:8081/gatravesso/v1/login', 
-        {
-            usuario: document.querySelector("#input_email").value, 
-            senha: document.querySelector("#input_password").value
-        }).then(data => {
-            console.log(data);
-        })
+        validarSenha('http://localhost:8081/gatravesso/v1/validarSenha', 
+            {
+                usuario: document.querySelector("#input_email").value, 
+                senha: document.querySelector("#input_password").value
+            })
     } else{
         msgErro.setAttribute('style', 'color: red');
         msgErro.setAttribute('style', 'display:block');
         msgErro.innerHTML = 'Preencha todos os campos corretamente e tente novamente'
     }
-
 }
 
 email.addEventListener('keyup', () => {
     if (email.value.length < 6) {
         email.setAttribute('style', 'color: red');
         email.setAttribute('style', 'border: 1px red solid');
-        label_email.setAttribute('style', 'color: red');
-        label_email.innerHTML = '*Insira no minimo 6 caracteres'  
+        labelEmail.setAttribute('style', 'color: red');
+        labelEmail.innerHTML = '*Insira no minimo 6 caracteres'  
         validEmail = false
     } else {
         email.setAttribute('style', 'color: black');
         email.setAttribute('style', 'border: 1px green solid');
-        label_email.innerHTML = ''
+        labelEmail.innerHTML = ''
         validEmail = true
     }
 })
 
 password.addEventListener('keyup', () => {
     if (password.value.length < 6) {
-        password.setAttribute('style', 'color: red');  
-        password.setAttribute('style', 'border: 1px red solid');
-        label_password.setAttribute('style', 'color: red');
-        label_password.innerHTML = '*Insira no minimo 6 caracteres'
-        validPassword = false
+         password.setAttribute('style', 'color: red');  
+         password.setAttribute('style', 'border: 1px red solid');
+         labelPassword.setAttribute('style', 'color: red');
+         labelPassword.innerHTML = '*Insira no minimo 6 caracteres'
+         validPassword = false
     } else {
         password.setAttribute('style', 'border: 1px green solid');
-        label_password.innerHTML = ''
+        labelPassword.innerHTML = ''
         validPassword = true
     }
 })
